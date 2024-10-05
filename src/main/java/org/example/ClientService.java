1dac6a2c2c38935dfe01db475d1fbed738f97281
Denis43t <javadenisstudy@gmail.com>
2024-10-05 18:57:01 +0200

    first commit

---
+++ src/main/java/org/example/ClientService.java
@@ -0,0 +1,93 @@
+package org.example;
+
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ClientService {
+    private static final String INSERT_INTO_CLIENT_TABLE =
+            "INSERT INTO client(name) VALUES (?)";
+    private static final String GET_BY_ID_FROM_CLIENT_TABLE =
+            "SELECT name FROM client WHERE id = ?";
+    private static final String UPDATE_CLIENT_NAME_TABLE =
+            "UPDATE client SET name= ? WHERE id = ?";
+
+
+    public long create(String name) throws SQLException {
+        long id=1;
+        Connection connection=Database.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+            if (name==null || name.length() <= 1 || name.length() >= 1000) {
+                throw new SQLException("Invalid Client Name");
+            }
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_INTO_CLIENT_TABLE);
+            preparedStatement.setString(1,name);
+            preparedStatement.executeUpdate();
+            ResultSet generatedKeys=preparedStatement.getGeneratedKeys();
+            while(generatedKeys.next()){
+            id=generatedKeys.getLong(1);
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        }
+        finally {
+            connection.setAutoCommit(true);
+        }
+        return id;
+    }
+    public String getById(long id) throws SQLException {
+        String resultReturn=null;
+        Connection connection=Database.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+            if (id==0) {
+                throw new SQLException("Invalid Client Id");
+            }
+            PreparedStatement preparedStatement = connection.prepareStatement(GET_BY_ID_FROM_CLIENT_TABLE);
+            preparedStatement.setLong(1,id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+            resultReturn=resultSet.getString(1);
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        }
+        finally {
+            connection.setAutoCommit(true);
+        }
+        return resultReturn;
+    }
+    public void setName(long id, String name) throws SQLException {
+        String resultReturn=null;
+        Connection connection=Database.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+            if (id==0) {
+                throw new SQLException("Invalid Client Id");
+            }
+            if (name==null || name.length() <= 1 || name.length() >= 1000) {
+                throw new SQLException("Invalid Client Name");
+            }
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_CLIENT_NAME_TABLE);
+            preparedStatement.setString(1,name);
+            preparedStatement.setLong(2,id);
+            preparedStatement.executeUpdate();
+
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        }
+        finally {
+            connection.setAutoCommit(true);
+        }
+    }
+}
---
+++ src/main/java/org/example/Database.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+public class Database {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5412/java-db";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "password";
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+    private static Database database;
+
+    static {
+        config.setJdbcUrl(DB_URL);
+        config.setUsername(DB_USER);
+        config.setPassword(DB_PASSWORD);
+        ds = new HikariDataSource(config);
+        Flyway flyway = Flyway.configure()
+                .dataSource(ds)
+                .locations("db/migrations")
+                .load();
+        flyway.migrate();
+    }
+
+    private Database() {
+    }
+
+    public static Database getInstance() {
+        if (database == null) {
+            database = new Database();
+        }
+        return database;
+    }
+
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+}
